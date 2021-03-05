4c18001c4c47c21521ce22c8f3e2909d8b2caa52
kees <vc@kees.site>
2021-03-04 17:38:27 -0800

    ads formula

---
+++ ads.rb
@@ -0,0 +1,17 @@
+class ads < Formula
+  desc "Interactive content utility"
+  homepage ""
+  url "https://github.com/kees-/ads/archive/v0.%CE%B11.tar.gz"
+  version "v0.α1"
+  sha256 "d6b06e9800c8bd1262bd8a490b08ea8b01a07af63162dedc4772b9b676724dea"
+  license ""
+
+  depends_on "zsh"
+  depends_on "gettext"
+  depends_on "ffmpeg"
+
+  def install
+	bin.install "ads"
+  end
+
+end
