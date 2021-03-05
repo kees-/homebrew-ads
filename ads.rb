821a6e9a7c7c745f51258ef8a316fe8d62566d06
kees <vc@kees.site>
2021-03-05 13:59:59 -0800

    Preparing homebrew rb

--- ads.rb
+++ ads.rb
@@ -2,9 +2,9 @@ class Ads < Formula
 
   desc "Interactive content utility"
   homepage "https://github.com/kees-/ads"
-  url "https://github.com/kees-/ads/archive/v0.%CE%B12.tar.gz"
-  version "v0.α2"
-  sha256 "8a70b2847e115b89ab2f2ae174dc5e31b5bd8c2d0d7e3c8edd7773a9a3f934b0"
+  url "https://github.com/kees-/ads/archive/v0.%CE%B13.tar.gz"
+  version "v0.α3"
+  sha256 "1005c8b16aaf55bc795c83434443cb4e029442341a8429c266028c197268764e"
   license "MIT"
 
   depends_on "zsh"
