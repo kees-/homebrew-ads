f33150ee53d8f788e0b3d63a01f6d88281a381aa
kees <vc@kees.site>
2021-03-04 20:21:28 -0800

    preparing homebrew rb

--- ads.rb
+++ ads.rb
@@ -1,6 +1,6 @@
 class Ads < Formula
   desc "Interactive content utility"
-  homepage ""
+  homepage "https://github.com/kees-/ads"
   url "https://github.com/kees-/ads/archive/v0.%CE%B11.tar.gz"
   version "v0.α1"
   sha256 "d6b06e9800c8bd1262bd8a490b08ea8b01a07af63162dedc4772b9b676724dea"
@@ -12,6 +12,8 @@ class Ads < Formula
 
   def install
 	bin.install "ads"
+	mkdir_p "${HOME}/.config/ads"
+	mv "lib/*", "${HOME}/.config/ads"
   end
 
 end
