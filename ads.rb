868efc0c96965abe55abe0c71fc2e7ab6b54e183
kees <vc@kees.site>
2021-03-06 12:00:18 -0800

    Preparing homebrew installation

--- ads.rb
+++ ads.rb
@@ -4,7 +4,7 @@ class Ads < Formula
   homepage "https://github.com/kees-/ads"
   url "https://github.com/kees-/ads/archive/v0.%CE%B13.tar.gz"
   version "v0.α3"
-  sha256 "eb93a194171188f04151d09d2b508762ccd46a7500be714302a69b848eaf05da"
+  sha256 "d3cf81968a557f0d12c216b6f2a21c4fb0e9a5afbf1a4c07e1628b51329304d6"
   license "MIT"
 
   depends_on "zsh"
@@ -13,8 +13,17 @@ class Ads < Formula
 
   def install
 	bin.install "ads"
-	lib.install "lib/ads"
-	system "./ads_install", "#{prefix}"
+	inreplace "ads_install", /prfx=;/, "prfx=\"#{prefix}\""
+	bin.install "ads_install"
+	lib.install "lib/ads.conf"
+	lib.install "lib/adt.conf"
+	lib.install "lib/klib.conf"
+	lib.install "lib/q.conf"
+	lib.install "lib/template"
+  end
+
+  def caveats
+	"\033[43;103m\xe2\x86\x92 Run \033[1mads_install\033[0;43;103m to finish the process and set up your instance.\033[K\n  Don't know how?\n  Just type \033[1mads_install\033[0;43;103m into your command prompt.\033[0m"
   end
 
 end
